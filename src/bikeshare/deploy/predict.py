6fc43886b8f0dee42f90c6aefb6a801a9e550565
vykuang <vykuang92@gmail.com>
2022-09-07 00:39:17 +0800

    lint and format; deployment WIP

--- src/bikeshare/deploy/predict.py
+++ src/bikeshare/deploy/predict.py
@@ -1,20 +1,23 @@
 """
 Retrieves and runs the latest version of the registered model
 """
-import os
 import logging
-from flask import Flask, request, jsonify
+import os
+
 import mlflow.pyfunc
+from flask import Flask, jsonify, request
 from mlflow.tracking import MlflowClient
 
 MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "sqlite:///mlflow.db")
 MLFLOW_EXP_NAME = os.getenv("MLFLOW_EXP_NAME", "TO-bikeshare-classifier")
 MLFLOW_REGISTERED_MODEL = os.getenv("MLFLOW_REGISTERED_MODEL", "TO-bikeshare-clf")
 
+
 def preprocess(ride):
-    logging.info('Preprocessing request')
+    logging.info("Preprocessing request")
     features = {}
     return features
+
 
 def retrieve():
     client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
@@ -40,16 +43,19 @@ def retrieve():
     )
     return model
 
+
 def predict(model, features):
-    """Given model and bikeshare trip data, return the predicted membership, 
+    """Given model and bikeshare trip data, return the predicted membership,
     as well as the associated probability
     """
     preds = model.predict(features)
     return float(preds[0])
 
-app = Flask('bikeshare-membership-prediction')
 
-@app.route('/predict', methods=['POST'])
+app = Flask("bikeshare-membership-prediction")
+
+
+@app.route("/predict", methods=["POST"])
 def predict_endpoint():
     """Joining the above funcs into one invocation triggered
     by an HTTP POST request
@@ -58,17 +64,15 @@ def predict_endpoint():
     """
     # from the POST request
     ride = request.get_json()
-    print('received POST')
-    logging.info('Received POST request')
+    print("received POST")
+    logging.info("Received POST request")
 
     features = preprocess(ride)
     preds = predict(features)
     result = {
-        'predicted_membership': preds,
-        'probability': preds[1],
-        'model_version': {},
+        "predicted_membership": preds,
+        "probability": preds[1],
+        "model_version": {},
     }
 
     return jsonify(result)
-
-
